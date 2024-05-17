d75545f617a5ab6316e296f32f2f1e4bc8c92d54
Максим Андрейчук <maximandreychuk1999@gmail.com>
2024-05-17 12:40:40 +0300

    add extra lesson of mod 1

---
+++ mod_1_extra_lesson/1.py
@@ -0,0 +1,17 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+sort_names = [i for i in students]
+sort_names.sort()
+
+average_rating = []
+for i in grades:
+    average_rating.append(sum(i)/len(i))
+
+answer = {}
+x = 0
+for i in range(x, len(sort_names)):
+    answer.update({sort_names[x]:average_rating[x]})
+    x += 1
+
+print(answer)
