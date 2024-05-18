4faa021ed82436ebf8b56aaad61e8bdc62ac2933
Максим Андрейчук <maximandreychuk1999@gmail.com>
2024-05-18 11:35:12 +0300

    добавил вывод длины строки

---
+++ mod_1_lesson_4/homework3.py
@@ -0,0 +1,7 @@
+my_string = input()
+print("Длина строки:", len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[:1])
+print(my_string[::-1][:1])
