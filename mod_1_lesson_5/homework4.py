aa6f3c5fa60953c3e238b403db7bd72b63bb60ec
Максим Андрейчук <maximandreychuk1999@gmail.com>
2024-05-18 11:44:32 +0300

    homework4.py

---
+++ mod_1_lesson_5/homework4.py
@@ -0,0 +1,13 @@
+immutable_var = ([1, 2], True, "radar")
+print("Кортеж: ", immutable_var)
+
+try:
+    immutable_var[1] = True
+except TypeError:
+    print("Кортеж является неизменяемым типом данных.")
+
+
+mutable_list = [1, 4, 2, 13, False, "Barfood"]
+print("Cписок: ", mutable_list)
+mutable_list[4], mutable_list[5] = True, "jdm"
+print("Измененный список: ", mutable_list)
