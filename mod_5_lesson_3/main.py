e4cccb9c79d42fed23d6b4cc18aea6222e4db533
Максим Андрейчук <maximandreychuk1999@gmail.com>
2024-05-25 12:10:10 +0300

    change return in eq method

--- mod_5_lesson_3/main.py
+++ mod_5_lesson_3/main.py
@@ -2,14 +2,18 @@ class Buiding():
     def __init__(self,
                  numberOfFloors: int,
                  buildingType: str) -> None:
-        self.numberOfFloors = int(numberOfFloors)
-        self.buildingType = str(buildingType)
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
 
-    def __eq__(self,) -> bool:
-        return self.numberOfFloors == self.buildingType
+    def __eq__(self, other) -> bool:
+        return (self.numberOfFloors == other.numberOfFloors
+                and self.buildingType == other.buildingType
+                )
 
 
-my_house = Buiding(2, "2")
+my_house = Buiding(2, "Office")
+my_house2 = Buiding(2, "Office")
+y_house = Buiding(9, "Brick")
 
-if my_house.__eq__():
-    print('Похожи')
+print(my_house == my_house2)
+print(my_house == y_house)
