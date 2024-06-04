12b98d90c29172b11f49240f137797d484029488
Максим Андрейчук <maximandreychuk1999@gmail.com>
2024-06-04 12:01:37 +0300

    datetime refactoring

--- mod_14_lesson_1/main.py
+++ mod_14_lesson_1/main.py
@@ -23,26 +23,26 @@ class SuperDate(datetime):
         self.date = datetime(year, month, day, hour)
 
     def get_season(self):
-        if self.month >= 3 and self.month <= 5:
+        if self.month in range(3, 6):
             return "Spring"
-        elif self.month >= 6 and self.month <= 8:
+        elif self.month in range(6, 9):
             return "Summer"
-        elif self.month >= 9 and self.month <= 11:
+        elif self.month in range(9, 12):
             return "Autumn"
         else:
             return "Winter"
 
     def get_time_of_day(self):
-        if self.day >= 6 and self.day <= 11:
+        if self.day in range(6, 12):
             return "Morning"
-        elif self.day >= 12 and self.day <= 17:
+        elif self.day in range(13, 18):
             return "Day"
-        elif self.day >= 18 and self.day <= 23:
+        elif self.day in range(18, 23):
             return "Evening"
         else:
             return "Night"
 
 
-s = SuperDate(2024, 1, 22, 12)
+s = SuperDate(2024, 1, 17, 10)
 print(s.get_season())
 print(s.get_time_of_day())
