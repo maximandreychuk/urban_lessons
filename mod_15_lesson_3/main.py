22b03dd726cf54160b328c03f4d0c7f496fed436
Максим Андрейчук <maximandreychuk1999@gmail.com>
2024-06-15 22:29:44 +0300

    add front camera like videocapture

--- mod_15_lesson_3/main.py
+++ mod_15_lesson_3/main.py
@@ -15,8 +15,7 @@
 
 import cv2
 import numpy as np
-
-cap = cv2.VideoCapture('eyes.mov')
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
@@ -26,11 +25,11 @@ while True:
         gray_frame, scaleFactor=1.3, minNeighbors=20)
 
     for (x, y, w, h) in eyes:
-        cv2.rectangle(frame, (x-350, y), (x+w, y + h), (77, 77, 77), 0)
-        region_of_interest = frame[y:y+h, x-350:x+w]
+        cv2.rectangle(frame, (x, y), (x+w, y + h), (77, 77, 77), 0)
+        region_of_interest = frame[y:y+h, x:x+w]
         blurred_region = cv2.GaussianBlur(
             region_of_interest, (77, 77), 77, 77)
-        frame[y:y+h, x-350:x+w] = blurred_region
+        frame[y:y+h, x:x+w] = blurred_region
 
     cv2.imshow('res', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
