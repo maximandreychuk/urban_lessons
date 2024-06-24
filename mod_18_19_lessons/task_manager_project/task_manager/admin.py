5d05ec1adaf6880574e55cb3e7746badea277e2e
Максим Андрейчук <maximandreychuk1999@gmail.com>
2024-06-24 13:36:34 +0300

    models, connections, warehouses, book=author

--- mod_18_19_lessons/task_manager_project/task_manager/admin.py
+++ mod_18_19_lessons/task_manager_project/task_manager/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
-from .models import Task, Comment, Page
+from .models import Author, Book, Task, Comment, Page, Publisher, Warehouse, Goods, Manager, Stocktaking, Shipper, GoodsWarehouse, TechicalMachine
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ['title__startswith', 'author__name']
+    list_filter = ['pub_date']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'birthdate']
+    list_filter = ['name']
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'address', 'phone__endswith']
+    list_filter = ['address']
 
 
 admin.site.register(Task)
 admin.site.register(Comment)
 admin.site.register(Page)
+
+admin.site.register(Warehouse)
+admin.site.register(Goods)
+admin.site.register(Manager)
+admin.site.register(Stocktaking)
+admin.site.register(Shipper)
+admin.site.register(GoodsWarehouse)
+admin.site.register(TechicalMachine)
--- mod_18_19_lessons/task_manager_project/task_manager/models.py
+++ mod_18_19_lessons/task_manager_project/task_manager/models.py
@@ -55,3 +55,89 @@ class Page(models.Model):
 
     def get_absolute_url(self):
         return f'/pages/{self.slug}'
+
+
+# далее - Домашнее задание по теме "Модели"
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birthdate = models.DateTimeField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    genre = models.CharField(max_length=50)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name='author_books')
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    books = models.ManyToManyField(Book, through='PublisherBook')
+
+
+class PublisherBook(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+
+# 5) Реализуйте ER модель базы данных склада в любом удобном вам редакторе(не менее 7 сущностей)
+
+class Warehouse(models.Model):
+    title = models.CharField(max_length=100)
+    goods_category = models.CharField(max_length=100)
+
+
+class Manager(models.Model):
+    name = models.CharField(max_length=100)
+    post = models.CharField(max_length=100)
+    warehouse = models.ForeignKey(
+        Warehouse, on_delete=models.CASCADE, related_name='manager_warehouses')
+
+
+class TechicalMachine(models.Model):
+    use = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+
+
+class Loader(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.CharField(max_length=100)
+    warehouse = models.ForeignKey(
+        Warehouse, on_delete=models.CASCADE, related_name='loader_warehouses')
+    techical_machine = models.ForeignKey(
+        TechicalMachine, on_delete=models.CASCADE, related_name='loader_technical_machines')
+
+
+class Shipper(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.IntegerField()
+
+
+class Goods(models.Model):
+    category = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    price = models.IntegerField()
+    shipper = models.ForeignKey(
+        Shipper, on_delete=models.CASCADE, related_name='goods_shipper')
+    warehouse = models.ManyToManyField(Warehouse, through='GoodsWarehouse')
+
+
+class Stocktaking(models.Model):
+    warehouse = models.ForeignKey(
+        Warehouse, on_delete=models.CASCADE, related_name='stocktaking_warehouses')
+    date = models.DateField()
+    difference = models.IntegerField()
+    done = models.BooleanField()
+
+
+class GoodsWarehouse(models.Model):
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
